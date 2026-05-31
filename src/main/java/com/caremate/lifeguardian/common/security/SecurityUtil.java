9e9fa029a693472a66ee1dd8b539127e85aef3d9
Park Jaeha <qkrwogk2233@gmail.com>
2026-05-31 13:25:48 +0900

    feature: jwt토큰 및 User의 Role, Status 추가. SecurityConfig 추가, BaseException/GlobalException 추가, SecurityUtil getCurrentUserId가 1000001만 나오게 함

---
+++ src/main/java/com/caremate/lifeguardian/common/security/SecurityUtil.java
@@ -0,0 +1,18 @@
+package com.caremate.lifeguardian.common.security;
+
+
+import com.caremate.lifeguardian.common.exception.BaseException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+
+public final class SecurityUtil {
+    private SecurityUtil(){
+        throw  new AssertionError("인스턴스 생성 불가");
+    }
+
+    public static Long getCurrentUserId() {
+        // 무조건 1000001번만 나오게 함
+        return 1000001L;
+    }
+}
---
+++ src/main/java/com/caremate/lifeguardian/member/domain/enums/Role.java
@@ -0,0 +1,5 @@
+package com.caremate.lifeguardian.member.domain.enums;
+
+public enum Role {
+    SALES, ADMIN
+}
