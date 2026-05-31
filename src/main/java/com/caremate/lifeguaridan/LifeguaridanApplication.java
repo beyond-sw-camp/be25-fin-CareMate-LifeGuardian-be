e2128cb6e7d31b335248a9acf5d9252399981ce6
Park Jaeha <qkrwogk2233@gmail.com>
2026-05-31 09:45:02 +0900

    feature: 초기 디렉터리 세팅

---
+++ src/main/java/com/caremate/lifeguaridan/LifeguaridanApplication.java
@@ -0,0 +1,13 @@
+package com.caremate.lifeguaridan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeguaridanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeguaridanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/caremate/lifeguaridan/LifeguaridanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caremate.lifeguaridan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeguaridanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
