018dfbb8e9bd42bff6e750c029ebac49e589f17d
Myang09 <xghujjk@gmail.com>
2026-06-07 21:16:38 +0900

    feat: 영업현황 API 추가

--- src/main/java/com/caremate/lifeguardian/sales/dto/response/SalesSummaryResponseDto.java
+++ src/main/java/com/caremate/lifeguardian/sales/dto/response/SalesSummaryResponseDto.java
@@ -1,11 +1,9 @@
 package com.caremate.lifeguardian.sales.dto.response;
 
-import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.type.Alias;
 
 @Getter
