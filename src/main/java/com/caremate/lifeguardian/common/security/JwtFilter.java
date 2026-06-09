fd83d9abee14efbe52a06c2376d6d438a2dcd5a3
wnstkd704 <wnstkd704@gamil.com>
2026-06-09 11:30:07 +0900

    refactor: OncePerRequestFilter의 메서드는 부모 클래스에서 매개변수들이 @NonNull로 선언되어 있어, 오버라이드한 메서드에도 어노테이션 추가

--- src/main/java/com/caremate/lifeguardian/common/security/JwtFilter.java
+++ src/main/java/com/caremate/lifeguardian/common/security/JwtFilter.java
@@ -6,6 +6,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,9 +25,9 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(
-            HttpServletRequest request,
-            HttpServletResponse response,
-            FilterChain filterChain
+            @NonNull HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
 
         try {
