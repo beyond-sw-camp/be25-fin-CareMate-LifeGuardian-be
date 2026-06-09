88f00ec86477bcf17513d21f830e1f934bf82c85
MyangD <xghujjk@gmail.com>
2026-06-09 11:42:18 +0900

    feat: 영업현황 목록 조회 기능 추가 (#20)
    * feat: 영업현황 API 추가
    * feat: 영업 현황 API를 인증 예외 경로에 추가

--- src/main/java/com/caremate/lifeguardian/config/SecurityConfig.java
+++ src/main/java/com/caremate/lifeguardian/config/SecurityConfig.java
@@ -59,6 +59,7 @@ public class SecurityConfig {
                                 "/api/v1/sales-users",
                                 "/api/v1/sales-users/*",
                                 "/api/v1/sales-users/*/status",
+                                "/api/v1/sales/*",
                                 "/api/v1/sales-users/*/transfer-customers",
                                 "/api/v1/sales-users/pii-secure",
                                 "/api/v1/branches/*/statistics/annual-contracts",
