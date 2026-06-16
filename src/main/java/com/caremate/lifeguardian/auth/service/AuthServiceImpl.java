c20789203457a636c83741bfd0c064afec15cdfd
wnstkd704 <wnstkd704@gmail.com>
2026-06-16 14:29:26 +0900

    feat: 로그인 응답에 소속 지점 정보 추가 (#27)
    * feat: getCurrentUserId 구현
    
    * refactor: 토큰 파싱할떄 유효성 검증 통합
    
    * refactor: 토큰 파싱할떄 유효성 검증 통합
    
    * refactor: 로그인 응답 branchId, branchName 추가
    
    * refactor: 스웨거 tag명 변경
    
    ---------
    
    Co-authored-by: wnstkd704 <wnstkd704@gamil.com>

--- src/main/java/com/caremate/lifeguardian/auth/service/AuthServiceImpl.java
+++ src/main/java/com/caremate/lifeguardian/auth/service/AuthServiceImpl.java
@@ -128,6 +128,8 @@ public class AuthServiceImpl implements AuthService {
 				.refreshToken(refreshToken)
 				.userId(user.getId())
 				.name(user.getName())
+				.branchId(user.getBranchId())
+				.branchName(user.getBranchName())
 				.role(role.name())
 				.isFirstLogin(user.getIsTempPassword())
 				.build();
