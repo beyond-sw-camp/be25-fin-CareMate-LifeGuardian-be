2ab9b01be27659910d987451d2ce39d8d5070cc4
wnstkd704 <wnstkd704@gmail.com>
2026-06-16 15:38:41 +0900

    feat: 최초 로그인 비밀번호 변경 구현 (#28)
    Co-authored-by: wnstkd704 <wnstkd704@gamil.com>

--- src/main/java/com/caremate/lifeguardian/auth/service/AuthService.java
+++ src/main/java/com/caremate/lifeguardian/auth/service/AuthService.java
@@ -1,6 +1,7 @@
 package com.caremate.lifeguardian.auth.service;
 
 
+import com.caremate.lifeguardian.auth.dto.request.InitialPasswordResetRequest;
 import com.caremate.lifeguardian.auth.dto.request.LoginRequest;
 import com.caremate.lifeguardian.auth.dto.response.AuthResultDto;
 
@@ -8,6 +9,13 @@ public interface AuthService {
 
 	AuthResultDto login(
 			LoginRequest request,
+			String ipAddress,
+			String userAgent
+	);
+
+	void resetInitialPassword(
+			Long userId,
+			InitialPasswordResetRequest request,
 			String ipAddress,
 			String userAgent
 	);
