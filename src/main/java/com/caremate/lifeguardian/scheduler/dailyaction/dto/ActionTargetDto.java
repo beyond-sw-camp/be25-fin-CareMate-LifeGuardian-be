02fe0263bd33192f8f68dbd5505caf8b90f7ecac
wnstkd704 <wnstkd704@gamil.com>
2026-06-18 16:31:33 +0900

    refactor: @Setter 삭제

--- src/main/java/com/caremate/lifeguardian/scheduler/dailyaction/dto/ActionTargetDto.java
+++ src/main/java/com/caremate/lifeguardian/scheduler/dailyaction/dto/ActionTargetDto.java
@@ -1,12 +1,10 @@
 package com.caremate.lifeguardian.scheduler.dailyaction.dto;
 
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDate;
 
 @Getter
-@Setter
 public class ActionTargetDto {
 
 	private Long customerId;
