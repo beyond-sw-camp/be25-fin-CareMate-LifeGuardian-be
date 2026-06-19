f171d75e79d8e37d35b7e8ae9d88197d655f8e03
Myang09 <xghujjk@gmail.com>
2026-06-19 16:16:27 +0900

    fix: @Builder 추가

--- src/main/java/com/caremate/lifeguardian/report/dto/request/ReportBulkSendRequest.java
+++ src/main/java/com/caremate/lifeguardian/report/dto/request/ReportBulkSendRequest.java
@@ -1,12 +1,17 @@
 package com.caremate.lifeguardian.report.dto.request;
 
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.AccessLevel;
 
 import java.util.List;
 
 @Getter
-@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ReportBulkSendRequest {
 
     // 선택 발송 대상 리포트 ID 목록. 비어 있으면 전체 발송으로 처리한다.
