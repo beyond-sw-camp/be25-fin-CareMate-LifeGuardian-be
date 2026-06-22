f862f41e6316d2b5bff4ece41c44b56b619ef10b
wnstkd704 <wnstkd704@gamil.com>
2026-06-22 10:45:10 +0900

    refactor: "/api/v1/auth/reissue" 경로 삭제

--- src/main/java/com/caremate/lifeguardian/config/SecurityConfig.java
+++ src/main/java/com/caremate/lifeguardian/config/SecurityConfig.java
@@ -75,7 +75,6 @@ public class SecurityConfig {
                                 "/api/v1/potential-customers/parent/search",
                                 "/api/v1/potential-customers/{potentialCustomerId}",
                                 "/api/v1/auth/login",
-                                "/api/v1/auth/reissue",
                                 "/api/v1/dashboard/**",
                                 "/api/v1/reports/**",
                                 "/api/v1/customer-reports/send/**",
